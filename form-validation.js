97315c93db0d54eac36c1e7a84821846e0e26e08
Esther AJAYI <estheradexdainty@gmail.com>
2024-02-23 18:59:43 +0100

    working on responsiveness

--- form-validation.js
+++ form-validation.js
@@ -57,3 +57,10 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('success-modal').style.display = 'none';
     }
 });
+
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('.nav');
+
+menuIcon.addEventListener('click', () => {
+    nav.classList.toggle('show');
+});
