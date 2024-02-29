14fcb65f204e177d0bb348da710eb46b7fe07946
Esther AJAYI <estheradexdainty@gmail.com>
2024-02-29 15:19:12 +0100

    form functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
