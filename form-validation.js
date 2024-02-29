4ea67d2618ae85e0d987fd950e31255f41f5fec9
Esther AJAYI <estheradexdainty@gmail.com>
2024-02-29 16:45:23 +0100

    form functionality

--- form-validation.js
+++ form-validation.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
             // If the form is valid, send data to the server using Fetch API
             const formData = new FormData(form);
 
-            fetch('/booking-flight', {
+            fetch('https://rdg-travels.onrender.com/booking-flight', {
                 method: 'POST',
                 body: formData
             })
