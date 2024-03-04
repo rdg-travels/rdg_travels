7a1d9cca974a96d48008c24a4e10063afc621693
Esther AJAYI <estheradexdainty@gmail.com>
2024-03-04 14:13:11 +0100

    study form

--- app.js
+++ app.js
@@ -22,9 +22,14 @@ app.use('/booking-flight', cors({
   allowedHeaders: ['Content-Type'],
 }));
 
-app.post('/booking-flight', (req, res) => {
-  const { flyingFrom, flyingTo, leavingOn, returningOn, fullName, email, phone, passengers } = req.body;
+app.use('/studying-abroad', cors({
+  origin: 'https://rdg-travels.netlify.app',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}));
 
+// Function to create transporter and verify connection configuration
+function createTransporter() {
   // Create a transporter using your email service credentials
   const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -34,15 +39,23 @@ app.post('/booking-flight', (req, res) => {
     },
   });
 
-// Verify the connection configuration
-transporter.verify(function(error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take our messages');
-  }
-});
+  // Verify the connection configuration
+  transporter.verify(function(error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Server is ready to take our messages');
+    }
+  });
 
+  return transporter;
+}
+
+app.post('/booking-flight', (req, res) => {
+  const { flyingFrom, flyingTo, leavingOn, returningOn, fullName, email, phone, passengers } = req.body;
+
+  // Create transporter
+  const transporter = createTransporter();
   // Fill the form
   const mailOptions = {
     from: email,
@@ -59,6 +72,32 @@ transporter.verify(function(error, success) {
     } else {
       console.log('Email sent:', info.response);
       res.send('Flight booked successfully!');
+    }
+  });
+});
+
+app.post('/study-abroad', (req, res) => {
+  const { countryOfInterest, fieldOfStudy, intendedProgram, fullName, email, phoneNumber } = req.body;
+
+  // Create transporter
+  const transporter = createTransporter();
+
+  // Fill the form
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL,
+    subject: 'New Study Abroad Inquiry',
+    text: `Country of Interest: ${countryOfInterest}\nField of Study: ${fieldOfStudy}\nIntended Program: ${intendedProgram}\nFull Name: ${fullName}\nEmail Address: ${email}\nPhone Number: ${phoneNumber}`,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('An error occurred while sending the email.');
+    } else {
+      console.log('Email sent:', info.response);
+      res.send('Study abroad inquiry submitted successfully!');
     }
   });
 });
