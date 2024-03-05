b43f9b30dfe1359fa160d3d590d723bef09626bb
Esther AJAYI <estheradexdainty@gmail.com>
2024-03-05 14:20:04 +0100

    study-form

--- form-validation.js
+++ form-validation.js
@@ -53,6 +53,56 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const studyForm = document.getElementById("study-form");
+    const successModal = document.getElementById("success-modal");
+
+    studyForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const formData = new FormData(studyForm);
+        const apiUrl = 'https://rdg-travels.onrender.com';
+
+        fetch(`${apiUrl}/studying-abroad`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(Object.fromEntries(formData)),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            // Check the Content-Type header to determine the response format
+            const contentType = response.headers.get('Content-Type');
+            if (contentType && contentType.includes('application/json')) {
+                // If the response is JSON, parse it and return the parsed data
+                return response.json();
+            } else {
+                // If the response is not JSON, return the response body as text
+                return response.text();
+            }
+        })
+        .then(data => {
+            // Display the success modal
+            successModal.style.display = 'block';
+            // Reset the flight form
+            studyForm.reset();
+        })
+        .catch(error => {
+            console.error(error);
+            alert("An error occurred while sending the message.");
+        });
+    });
+
+    const closeButton = document.getElementById("closeButton");
+    closeButton.addEventListener("click", function() {
+        // Close the modal when the close button is clicked
+        closeModal();
+    });
+});
+
 function validateForm(form) {
     let isValid = true;
 
